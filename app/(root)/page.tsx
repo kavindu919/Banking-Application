8771b4039b135501fa9164e3998a14b3ef21cb52
kavindu919 <jayakodyk2@gmail.com>
2024-05-15 13:28:18 +0530

    start to making home ui added counter for display balance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Kavindu" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently"
+          />
+          {/* current balance and bank status  */}
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1236.5}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
